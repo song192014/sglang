cbd2f194909c4c137d168c25487caf2f76b521fa
Songtienan (A) <s30040863@china.huawei.com>
2025-12-02 11:46:57 +0800

    add lm head fp32

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -19,6 +19,8 @@ import json
 import logging
 import os
 import random
+import socket
+import sys
 import tempfile
 from typing import List, Literal, Optional, Union
 
@@ -169,6 +171,7 @@ class ServerArgs:
     quantization: Optional[str] = None
     quantization_param_path: Optional[str] = None
     kv_cache_dtype: str = "auto"
+    enable_fp32_lm_head: bool = False
 
     # Memory and scheduling
     mem_fraction_static: Optional[float] = None
@@ -1295,6 +1298,11 @@ class ServerArgs:
             default=ServerArgs.kv_cache_dtype,
             choices=["auto", "fp8_e5m2", "fp8_e4m3"],
             help='Data type for kv cache storage. "auto" will use model data type. "fp8_e5m2" and "fp8_e4m3" is supported for CUDA 11.8+.',
+        )
+        parser.add_argument(
+            "--enable-fp32-lm-head",
+            action="store_true",
+            help='If set, the LM head outputs (logits) are in FP32.',
         )
 
         # Memory and scheduling
@@ -2969,6 +2977,10 @@ def prepare_server_args(argv: List[str]) -> ServerArgs:
     parser = argparse.ArgumentParser()
     ServerArgs.add_cli_args(parser)
     raw_args = parser.parse_args(argv)
+    if raw_args.enable_fp32_lm_head:
+        print("FP32 LM head enabled.")
+    else:
+        print("FP32 LM head disabled.")
     server_args = ServerArgs.from_cli_args(raw_args)
     return server_args
 
